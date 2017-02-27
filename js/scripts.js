8422b26853dfaa5d20bc9a185980415a653b1ddc
Ben <benschenkenberger@gmail.com>
2017-02-27 13:30:46 -0800

    test responses to form completion: Add html to test responses to questionnaire. Launch and reconsider are working; reevaluate not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,33 @@
 $(function(){
   $("#stageCommit").submit(function(){
     event.preventDefault();
+
     var score = 0;
-  score += parseInt($("input:radio[name=team]:checked").val());
-  score += parseInt($("input:radio[name=probSolve]:checked").val());
-  score += parseInt($("input:radio[name=commit]:checked").val());
-  score += parseInt($("input:radio[name=why]:checked").val());
-  score += parseInt($("input:radio[name=personality]:checked").val());
-  console.log(score);
+    score += parseInt($("input:radio[name=team]:checked").val());
+    score += parseInt($("input:radio[name=probSolve]:checked").val());
+    score += parseInt($("input:radio[name=commit]:checked").val());
+    score += parseInt($("input:radio[name=why]:checked").val());
+    score += parseInt($("input:radio[name=personality]:checked").val());
+    console.log(score);
 
+    if (score>=12) {
+    $(".launch").show()
+    $(".reevalute").hide()
+    $(".reconsider").hide()
+    }
 
-  if (score>=12)
-  {$("#answer1").show()
-  $("#answer2").hide()
-  $("#answer3").hide()}
+    else if ((score<=11) && (score>=9)) {
+    $(".reevalute").show()
+    $(".launch").hide()
+    $(".reconsider").hide()
+    }
 
-  else if (score>=9 && score<=11)
-  {$("#answer2").show()
-  $("#answer1").hide()
-  $("#answer3").hide()}
+    else if (score<9) {
+    $(".reconsider").show()
+    $(".reevaluate").hide()
+    $(".launch").hide()
+    }
 
-  else if (score<9)
-  {$("#answer3").show()
-  $("#answer2").hide()
-  $("#answer1").hide()}
-
-
-
-});
+  });
 
 });
